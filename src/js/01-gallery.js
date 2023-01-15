d4383cc9908954a66005b0b3548b07a535e2918d
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-16 01:23:20 +0200

    hw-08.js.task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const marcup = galleryItems
+  .map(
+    item => `<a class="gallery__item" 
+    href="${item.original}">
+  <img class="gallery__image"
+  src="${item.preview}"
+   alt="${item.description}" />
+</a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', marcup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
