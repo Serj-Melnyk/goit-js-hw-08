e9fc001c123135eb686f9d1456d1a582b207cd49
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-16 02:06:00 +0200

    hw-08.js.task-2.start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const frame = document.querySelector('#vimeo-player');
+console.log(frame);
+
+const player = new Player('frame', {});
+
+player.on('timeupdate', function () {
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+});
